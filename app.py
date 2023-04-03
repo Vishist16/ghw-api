1a84cb3e33551db91da29b4d372e680c4ffd6799
Vishist Singh Solanki <vishistsingh16gh@gmail.com>
2023-04-04 02:28:09 +0530

    added POST method

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ hackathons = {
 def hello_world():
     return "<p>Hello API week hackers!!</p>"
 
-@app.route('/hackathons', methods=['GET'])
+@app.route('/hackathons', methods=['GET', 'POST'])
 def getHackathons():
     if request.method == 'POST':
         hackathons["New Hackathon"] = request.json
